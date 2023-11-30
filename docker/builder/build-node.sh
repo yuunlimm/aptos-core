4d1042e9490fab3e20715551fe9ae3c12ddc8e73
Perry Randall <perryjrandall@gmail.com>
2023-11-30 10:44:35 -0800

    [forge] Unstrip forge binary
    Unfortunately stripping removes our ability to get stack traces
    
    And figure out a way to distribute the symbols or symbolicate separately
    
    Test Plan: tools image on the PR is a reasonable size (below 10GB)
    
    Longer term we want to move the individual tools separate images

--- docker/builder/build-node.sh
+++ docker/builder/build-node.sh
@@ -11,20 +11,21 @@ echo "PROFILE: $PROFILE"
 echo "FEATURES: $FEATURES"
 echo "CARGO_TARGET_DIR: $CARGO_TARGET_DIR"
 
+PACKAGES="-p aptos-node -p aptos-forge-cli"
+
 # Build and overwrite the aptos-node binary with features if specified
 if [ -n "$FEATURES" ]; then
     echo "Building aptos-node with features ${FEATURES}"
-    cargo build --profile=$PROFILE --features=$FEATURES -p aptos-node "$@"
+    cargo build --profile=$PROFILE --features=$FEATURES $PACKAGES "$@"
 else 
     # Build aptos-node separately
-    cargo build --locked --profile=$PROFILE \
-        -p aptos-node \
-        "$@"
+    cargo build --locked --profile=$PROFILE $PACKAGES "$@"
 fi
 
 # After building, copy the binaries we need to `dist` since the `target` directory is used as docker cache mount and only available during the RUN step
 BINS=(
     aptos-node
+    forge
 )
 
 mkdir dist
